8e681827b4c9a66719714afa8188eb868f21d014
gauravkansal01 <gkansal@gmail.com>
2024-04-18 22:03:34 +0530

    18-04

---
+++ 1_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "gaurav.kansal@abesit.edu.in";
+var accountPassword = "12345";
+accountCity = "Ghaziabad";
+
+//accountId = 2;
+
+accountEmail = "abc@abc.com";
+accountPassword = "12345678";
+accountCity = "noida";
+
+console.log(accountId);
+console.log([accountEmail, accountId, accountPassword]);
